c78bbffdddb33288e56bff419563a135e1be0d00
kquote03 <kquote03@criptext.com>
2021-10-02 00:19:46 +0400

    patch 5

--- drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
@@ -2249,6 +2249,7 @@ gfx_v7_0_raster_config(struct amdgpu_device *adev, u32 *rconf, u32 *rconf1)
 			  PKR_YSEL(1) | SE_MAP(2) | SE_XSEL(2) |
 			  SE_YSEL(2);
 		*rconf1 |= 0x0;
+		break;
 	case CHIP_GLADIUS:
 		*rconf |= 0x2a00161a;
 		*rconf1 |= 0x0000002e;
@@ -5018,7 +5019,7 @@ static void gfx_v7_0_gpu_early_init(struct amdgpu_device *adev)
 		adev->gfx.config.max_tile_pipes = 8; // Verified
 		adev->gfx.config.max_cu_per_sh = 9; // Probably OK
 		adev->gfx.config.max_sh_per_se = 1; // Verified
-		adev->gfx.config.max_backends_per_se = 1; // Probably OK?
+		adev->gfx.config.max_backends_per_se = 2; // Probably OK?
 		adev->gfx.config.max_texture_channel_caches = 4; // ??
 		adev->gfx.config.max_gprs = 256;
 		adev->gfx.config.max_gs_threads = 32; // ??
